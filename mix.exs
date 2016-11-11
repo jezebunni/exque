3e225843e5b8eb9d5cfeba608cc18d4681c2f98a
Dan Matthews <dan@matthews.es>
2016-11-11 14:32:43 -0500

    Initial Commit

---
+++ mix.exs
@@ -0,0 +1,48 @@
+defmodule Exque.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :exque,
+      description: "Emque compatible producers and consumers for Elixir.",
+      package: package,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+    ]
+  end
+
+  def application do
+    [applications: [:logger]]
+  end
+
+  defp deps do
+    [
+      {:timex, "~> 3.0"},
+      {:uuid, "~> 1.1"},
+      {:poison, "~> 3.0"},
+      {:ex_doc, "~> 0.14", only: :dev},
+      {
+        :amqp_client,
+        git: "https://github.com/dsrosario/amqp_client.git",
+        branch: "erlang_otp_19",
+        override: true
+      },
+      {:amqp, "0.1.5"},
+      {:uuid, "~> 1.1"}
+    ]
+  end
+
+  defp package do
+    [
+      contributors: ["Dan Matthews", "Josh Lee"],
+      licenses: ["MIT"],
+      links: %{
+        github: "https://github.com/localvore-today/exque.git"
+      },
+      files: ~w(lib mix.exs README.md)
+    ]
+  end
+end
